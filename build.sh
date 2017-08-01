8e78d592307025f5962412833e428818ac229b26
Euan T <euantorano@users.noreply.github.com>
2017-08-01 12:44:44 +0100

    Use printf rather than echo

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@ echo "Building with DMD..."
 rm D/{csv_test,csv_test_ldc,*.o}
 dmd -O -release -inline -boundscheck=off -of=./D/csv_test ./D/csv_test.d
 
-echo "\nBuilding with LDC..."
+printf "\nBuilding with LDC..."
 ldc2 -of=./D/csv_test_ldc -O -release -boundscheck=off ./D/csv_test.d
 
-echo "\nBuilding with Nim"
+printf "\nBuilding with Nim"
 rm Nim/csv_test
 rm -Rf Nim/nimcache
 nim c -d:release -o:./Nim/csv_test ./Nim/csv_test.nim
 
-echo "\nBuilding with C"
+printf "\nBuilding with C"
 rm C/fast
 make -C C/
