d4b549e23ac9076e4752b36b758ec81c628933c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-30 15:46:20 +0000

    Deploy euantorano/faster-command-line-tools-in-nim to github.com/euantorano/faster-command-line-tools-in-nim.git:gh-pages

--- resources/run.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-( time /app/csv_test /data/ngrams.tsv 1 2 ) > /output/$1 2>&1
--- resources/run_python.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-( time python /app/csv_test.py /data/ngrams.tsv 1 2 ) > /output/$1 2>&1
